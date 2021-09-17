3120db05d42fe61b4d3f488c0ef3231fa593c9d6
sharathnair9999 <sharathnair9999@gmail.com>
2021-09-17 21:09:37 +0530

    added all elements

---
+++ app.js
@@ -0,0 +1,7 @@
+var billAmount  = document.querySelector("#bill")
+var cashGiven = document.querySelector("#cash-given")
+var checkBtn = document.querySelector("#check")
+
+checkBtn.addEventListener("click", function(){
+  console.log("Clicked check")
+})
